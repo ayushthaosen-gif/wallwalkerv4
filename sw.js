39dc853e3e871f6d8ab1c2b01028e9388f6746d2
ayushthaosen-gif <ayushthaosen@gmail.com>
2026-03-01 14:30:26 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+// This file makes your app work offline!
+const CACHE_NAME = 'pathrater-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
